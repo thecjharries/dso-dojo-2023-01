a7ec3ec448a3200a1064aefdbf9c98c15c7be1fd
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-01 17:01:54 -0600

    Add very basic redis test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use redis::{Client, Commands, Connection, RedisResult};
+
 fn main() {
-    println!("Hello, world!");
+    test_redis().unwrap();
+}
+
+fn test_redis() -> RedisResult<()> {
+    let client = Client::open("redis://127.0.0.1/")?;
+    let mut connection: Connection = client.get_connection()?;
+    connection.set("foo", "bar")?;
+    let value: String = connection.get("foo")?;
+    println!("value: {}", value);
+    Ok(())
 }

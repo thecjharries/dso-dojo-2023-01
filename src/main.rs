51b8cc4eb1e8438f4630c7ce69cfe24de7a63cf5
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-01 17:59:42 -0600

    Make the connection string an env var

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use lazy_static::lazy_static;
 use r2d2_redis::r2d2::Pool;
 use r2d2_redis::RedisConnectionManager;
 use rocket::{build, get, launch, routes};
+use std::env::var;
+
+lazy_static! {
+    static ref REDIS_CONNECTION_STRING: String =
+        var("REDIS_CONNECTION_STRING").unwrap_or("redis://localhost:6379".to_string());
+}
 
 fn redis_pool() -> Pool<RedisConnectionManager> {
-    let manager = RedisConnectionManager::new("redis://localhost:6379").unwrap();
+    let manager = RedisConnectionManager::new(REDIS_CONNECTION_STRING.as_str()).unwrap();
     Pool::new(manager).unwrap()
 }
 

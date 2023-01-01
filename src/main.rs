01d015669a509a549876995450c24c7bf6424f18
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-01 17:49:47 -0600

    Create redis_pool to connect to redis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use r2d2_redis::r2d2::Pool;
+use r2d2_redis::RedisConnectionManager;
 use rocket::{build, get, launch, routes};
+
+fn redis_pool() -> Pool<RedisConnectionManager> {
+    let manager = RedisConnectionManager::new("redis://localhost:6379").unwrap();
+    Pool::new(manager).unwrap()
+}
 
 #[get("/")]
 fn index() -> &'static str {

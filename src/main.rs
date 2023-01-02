82e3172fb7254825151e9820148b44aeaee9d1ff
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-01 18:24:26 -0600

    Attempt to implement FromRequest on RedisConnection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use lazy_static::lazy_static;
-use r2d2_redis::r2d2::Pool;
+use r2d2_redis::r2d2::{Pool, PooledConnection};
 use r2d2_redis::RedisConnectionManager;
-use rocket::{build, get, launch, routes};
+use rocket::http::Status;
+use rocket::outcome::try_outcome;
+use rocket::request::{self, FromRequest, Request};
+use rocket::{async_trait, build, get, launch, routes, State};
 use std::env::var;
 
 lazy_static! {
@@ -12,6 +15,22 @@ lazy_static! {
 fn redis_pool() -> Pool<RedisConnectionManager> {
     let manager = RedisConnectionManager::new(REDIS_CONNECTION_STRING.as_str()).unwrap();
     Pool::new(manager).unwrap()
+}
+
+struct RedisConnection(PooledConnection<RedisConnectionManager>);
+type RedisConnectionPool = Pool<RedisConnectionManager>;
+
+#[async_trait]
+impl<'r> FromRequest<'r> for RedisConnection {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        let pool = try_outcome!(request.guard::<&State<RedisConnectionPool>>().await);
+        match pool.get() {
+            Ok(conn) => request::Outcome::Success(RedisConnection(conn)),
+            Err(_) => request::Outcome::Failure((Status::ServiceUnavailable, ())),
+        }
+    }
 }
 
 #[get("/")]
